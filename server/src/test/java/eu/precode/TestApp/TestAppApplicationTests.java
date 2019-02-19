7669ece6906418a3fb24760497223ef5a441ccd0
PreCodeEU <germerott@precode.eu>
2019-02-19 09:25:18 +0100

    Testing with raw json makes the same error

--- server/src/test/java/eu/precode/TestApp/TestAppApplicationTests.java
+++ server/src/test/java/eu/precode/TestApp/TestAppApplicationTests.java
@@ -1,7 +1,8 @@
 package eu.precode.TestApp;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.apache.commons.codec.binary.Base64;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.json.JSONObject;
 import org.junit.Assert;
 import org.junit.Test;
@@ -23,26 +24,36 @@ public class TestAppApplicationTests {
     private RestTemplate restTemplate = new RestTemplate();
 
 
-	@Test
-	public void testOauthToken(){
+    @Test
+    public void testOauthToken() {
 
         String plainCreds = "client:topsecret";
         byte[] plainCredsBytes = plainCreds.getBytes();
         byte[] base64CredsBytes = Base64.encodeBase64(plainCredsBytes);
         String base64Creds = new String(base64CredsBytes);
 
-        Object oauthRequest = new OauthRequest("Lukas", "test123", "password");
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Authorization", "Basic "+ base64Creds);
-		HttpEntity<Object> reqData = new HttpEntity<>(oauthRequest, headers);
-
-		ResponseEntity<Object> entity  = restTemplate.exchange("http://localhost:8080/oauth/token", HttpMethod.POST,
-                reqData,
-                Object.class);
-
-	}
+        ObjectMapper mapper = new ObjectMapper();
 
 
+        OauthRequest oauthRequest = new OauthRequest("Lukas", "test123", "password");
+        String json;
+
+        try {
+            json = mapper.writeValueAsString(oauthRequest);
+            HttpHeaders headers = new HttpHeaders();
+            headers.add("Authorization", "Basic " + base64Creds);
+            headers.add("Content-Type", "application/json");
+            HttpEntity<Object> reqData = new HttpEntity<>(json, headers);
+
+            ResponseEntity<Object> entity = restTemplate.exchange("http://localhost:8080/oauth/token", HttpMethod.POST,
+                    reqData,
+                    Object.class);
+
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+
+    }
 }
 
