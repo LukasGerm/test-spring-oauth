cf909425e0c6a16d2d758ca09b764ae0c4c1336a
PreCodeEU <germerott@precode.eu>
2019-02-19 09:33:34 +0100

    Changed media type, because needed

--- server/src/test/java/eu/precode/TestApp/TestAppApplicationTests.java
+++ server/src/test/java/eu/precode/TestApp/TestAppApplicationTests.java
@@ -8,10 +8,7 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
@@ -38,20 +35,17 @@ public class TestAppApplicationTests {
         OauthRequest oauthRequest = new OauthRequest("Lukas", "test123", "password");
         String json;
 
-        try {
-            json = mapper.writeValueAsString(oauthRequest);
+
             HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
             headers.add("Authorization", "Basic " + base64Creds);
-            headers.add("Content-Type", "application/json");
-            HttpEntity<Object> reqData = new HttpEntity<>(json, headers);
+            HttpEntity<Object> reqData = new HttpEntity<>(oauthRequest, headers);
 
             ResponseEntity<Object> entity = restTemplate.exchange("http://localhost:8080/oauth/token", HttpMethod.POST,
                     reqData,
                     Object.class);
 
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-        }
+
 
 
     }
